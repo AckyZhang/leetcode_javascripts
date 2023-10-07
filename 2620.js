a7babcc54e98c3cf1eed698f2edc862fe2ce0bc0
JiajunZhang <acediazhang@gmail.com>
2023-10-07 06:07:03 -0500

    using console.log() to check output

--- 2620.js
+++ 2620.js
@@ -3,13 +3,13 @@
  * @return {Function} counter
  */
 var createCounter = function(n) {
-    return function(n) {
+    return function() {
         return n++
     };
 };
 
 
 const counter = createCounter(10)
-counter() // 10
-counter() // 11
-counter() // 12
+console.log(counter()) // 10
+console.log(counter()) // 11
+console.log(counter()) // 12
